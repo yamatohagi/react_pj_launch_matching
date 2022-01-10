889bf5972ff973e5a0913408f587ed155318d646
yamatohagi <la.luce.ymt0326@gmail.com>
2022-01-10 17:16:14 +0900

    done validetion

--- src/view/Create.js
+++ src/view/Create.js
@@ -1,13 +1,13 @@
-import * as React from "react";
+import React, { useState } from "react";
 import "../App.css";
 import { TextField, Button, Box, LinearProgress, Grid } from "@mui/material";
 import AdapterDateFns from "@mui/lab/AdapterDateFns";
 import LocalizationProvider from "@mui/lab/LocalizationProvider";
 import DateTimePicker from "@mui/lab/DateTimePicker";
-import ButtonUnstyled, { buttonUnstyledClasses } from "@mui/base/ButtonUnstyled";
 import Header from "../components/Header";
 import { Link, useHistory } from "react-router-dom";
-import CustomButtonRoot from "../components/CustomButtonRoot";
+
+import Validation from "../components/Validation";
 
 function Create() {
   const history = useHistory();
@@ -15,9 +15,42 @@ function Create() {
 
   const [value, setValue] = React.useState(new Date());
 
-  const SvgButton = React.forwardRef(function SvgButton(props, ref) {
-    return <ButtonUnstyled {...props} component={CustomButtonRoot} ref={ref} />;
-  });
+
+  const state = {
+    info: {
+      email: "",
+      name: "",
+    },
+    message: {
+      email: "",
+      name: "",
+    },
+    loading: false,
+  };
+  const [values, setState] = useState(state);
+
+  const handleChange = (event) => {
+    const key = event.target.name;
+    const value = event.target.value;
+    const { info, message } = values;
+
+    setState({
+      info: { ...info, [key]: value },
+      message: { ...message, [key]: Validation.formValidate(key, value) },
+      loading: false,
+    });
+  };
+  const canSubmit = () => {
+    const validInfo =
+      Object.values(values.info).filter((value) => {
+        return value === "";
+      }).length === 0;
+    const validMessage =
+      Object.values(values.message).filter((value) => {
+        return value !== "";
+      }).length === 0;
+    return validInfo && validMessage && !values.loading;
+  };
 
   return (
     <form>
@@ -35,13 +68,37 @@ function Create() {
             <Grid sm={2} />
             <Grid xs={12} sm={8}>
               <div style={{ height: 100 }}>
-                <TextField required id="mail-address" name="mail" label="メールアドレス" variant="standard" />
+                <TextField
+                  style={{ width: 260 }}
+                  error={values.message.email}
+                  helperText={values.message.email}
+                  required
+                  id="mail-address"
+                  type="email"
+                  name="email"
+                  value={values.info.email}
+                  label="メールアドレス"
+                  variant="standard"
+                  onChange={(event) => handleChange(event)}
+                />
               </div>
               <div style={{ height: 100 }}>
-                <TextField required id="name" name="name" label="名前" variant="standard" />
+                <TextField
+                  error={values.message.name}
+                  helperText={values.message.name}
+                  value={values.info.name}
+                  onChange={(event) => handleChange(event)}
+                  required
+                  style={{ width: 260 }}
+                  id="name"
+                  name="name"
+                  label="名前   (山田 太郎)"
+                  variant="standard"
+                />
               </div>
               <div style={{ height: 100 }}>
-                <TextField required id="department-name" name="department-name" label="事業部" variant="standard" />
+                <TextField style={{ width: 260 }}
+                required id="department-name" name="department-name" label="事業部" variant="standard" />
               </div>
               <div style={{ height: 100 }}>
                 {/* <TextField required id="standard-required" label="希望日" variant="standard" /> */}
@@ -64,32 +121,38 @@ function Create() {
           <LinearProgress />
         </Box>
         <br></br>
-        <SvgButton
+        <Button
           type="submit"
           id="submit-button"
+          variant="contained"
+          color="primary"
+          size="large"
           onClick={() => {
-            document.getElementById("submit-button").style.visibility = "hidden";
-            document.getElementById("load-status").style.visibility = "visible";
-            const url = "https://jsonplaceholder.typicode.com/posts/";
-            const requestOptions = {
-              method: "POST",
-              headers: { "Content-Type": "application/json" },
-              body: JSON.stringify({
-                Name: document.getElementById("name").value,
-                Mail: document.getElementById("mail-address").value,
-                DepartmentName: document.getElementById("department-name").value,
-                DateTime: document.getElementById("date-time").value,
-              }),
-            };
-            fetch(url, requestOptions)
-              .then((response) => response.json())
-              .then((responseJson) => {
-                onClickSection(responseJson);
-              });
+            if (canSubmit()) {
+              document.getElementById("submit-button").style.visibility = "hidden";
+              document.getElementById("load-status").style.visibility = "visible";
+              const url = "https://jsonplaceholder.typicode.com/posts/";
+              const requestOptions = {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                  Name: document.getElementById("name").value,
+                  Mail: document.getElementById("mail-address").value,
+                  DepartmentName: document.getElementById("department-name").value,
+                  DateTime: document.getElementById("date-time").value,
+                }),
+              };
+              fetch(url, requestOptions)
+                .then((response) => response.json())
+                .then((responseJson) => {
+                  onClickSection(responseJson);
+                });
+            } else {
+            }
           }}
         >
           Go!! lunch!!
-        </SvgButton>
+        </Button>
         <br></br>
       </div>
     </form>
